53d99cdb127874a2974da60bdc6ec2a5ca4456e2
t-laptop <tpbush@gmail.com>
2020-01-07 17:27:15 -0800

    added import BinsegHistory

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -6,6 +6,6 @@ Offline change point detection for Python
 from __future__ import absolute_import
 from .exceptions import NotEnoughPoints
 from .datasets import pw_constant, pw_normal, pw_linear, pw_wavy
-from .detection import (Binseg, BottomUp, Dynp, Omp, OmpK, Pelt, Window,
+from .detection import (Binseg, BinsegHistory, BottomUp, Dynp, Omp, OmpK, Pelt, Window,
                         GreedyAR, GreedyLinear)
 from .show import display
